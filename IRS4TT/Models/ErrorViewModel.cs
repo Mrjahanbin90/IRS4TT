b2a2e365382e13d2926ff254204fab62b14128b6
Mohammad Javad Jahanbin <MrJJahanbin@hotmail.com>
2025-07-14 15:23:35 +0330

    Add project files.

---
+++ IRS4TT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IRS4TT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
